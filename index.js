59c289470b4b558d1be75cd35e75b9c0c4e745e7
inga <ogutuinga@gmail.com>
2026-03-12 16:04:57 +0300

    my first coding

---
+++ index.js
@@ -0,0 +1,33 @@
+ 
+
+//  arrays = list of items
+// array methods
+//array properties
+// const array = []
+const names = []
+
+// console.log(names)
+
+// console.log(names.length)
+
+// array methods
+// add  push, unshift
+// delete  pop, remove, shift 
+// sort- sort
+
+names.push("james")
+names.push("mark")
+names.push("inga")
+
+names.pop()
+names.sh
+
+
+// for(i = 0; i<=names.length; i++){
+// console.log(names[i])
+// }
+
+names.forEach((name) =>{
+    console.log(name)
+})
+
